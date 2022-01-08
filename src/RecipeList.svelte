dd7c375abe176a6411196cd118af0a6e5955a6d4
asullivan2025 <a.sullivan.2025@gmail.com>
2022-01-08 18:10:02 +0000

    use diffrent starter template

---
+++ src/RecipeList.svelte
@@ -0,0 +1,24 @@
+<script>
+	let recipes = [];
+
+	fetch("http://localhost:3000/recipes")
+		.then((response) => response.json())
+		.then((data) => {
+			for (const item of data) {
+				console.log(item.name);
+			}
+
+			recipes=data;
+		});
+</script>
+
+<div><h1>recipes</h1></div>
+
+<div>
+	{#each recipes as recipe}
+		<div>{recipe.name}</div>
+	{/each}
+</div>
+
+<style>
+</style>
