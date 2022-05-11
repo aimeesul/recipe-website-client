ab3889b75fc8ea532541a3552f90f35e98ed3d13
asullivan2025 <a.sullivan.2025@gmail.com>
2022-05-11 14:37:13 +0000

    Add log out and switch account buttons

--- src/LoginLink.svelte
+++ src/LoginLink.svelte
@@ -1,18 +1,30 @@
 <script>
     import { user } from "./stores";
+    import { accessToken } from "./stores";
     import LinkButton from "./LinkButton.svelte";
     import { url } from "@roxi/routify";
+    import Button from "./Button.svelte";
+
+    function logout(event) {
+        user.set(null);
+        accessToken.set(null);
+        event.preventDefault();
+    }
 </script>
 
 {#if $user}
     <div>logged in as {$user.userName}</div>
     <div>
-        <LinkButton secondary href={$url("/recipes/addNew")}>Add new recipe</LinkButton
+        <LinkButton secondary href={$url("/recipes/addNew")}
+            >Add new recipe</LinkButton
+        >
+        <LinkButton secondary href="http://localhost:3000/api/authentication/google/start" 
+            >Switch account</LinkButton
+        >
+        <LinkButton secondary href="http://localhost:5000" on:click={logout}
+            >Log out</LinkButton
         >
     </div>
-    <a href="http://localhost:3000/api/authentication/google/start"
-        >Switch account</a
-    >
 {:else}
     <a href="http://localhost:3000/api/authentication/google/start"
         >Log in with Google</a
